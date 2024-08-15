cd4126dfe1d679ecb2158469a226ed11d3944d8a
airondevt <airondev@gmail.com>
2024-08-15 10:50:55 +0100

    fix js issue

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,7 +14,7 @@
     rel="stylesheet" />
   <link rel="stylesheet"
     href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
-  <script src="main.js"></script>
+  <script src="assets/main.js"></script>
 </head>
 
 <body>
