a034f57644224733d1f35d2c06964633b47bd105
airondevt <airondev@gmail.com>
2024-10-10 18:30:29 +0100

    make email verification code 4 characters

--- routes/console.php
+++ routes/console.php
@@ -10,4 +10,4 @@ Artisan::command('inspire', function () {
 
 
   
-Schedule::command('queue:listen')->everySecond()->withoutOverlapping();
+Schedule::command('queue:work')->everySecond()->withoutOverlapping();
