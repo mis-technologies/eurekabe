36588dabd7e6b37adc7ea31d1196065fc0cdacdb
airondevt <airondev@gmail.com>
2024-10-24 13:36:25 +0100

    init

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,11 +3,13 @@
 use Modules\Admin\Providers\AdminServiceProvider;
 use Modules\Auth\Providers\AuthServiceProvider;
 use Modules\Exam\Providers\ExamServiceProvider;
+use Modules\Student\Providers\StudentServiceProvider;
 
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\FolioServiceProvider::class,
     AdminServiceProvider::class,
     AuthServiceProvider::class,
-    ExamServiceProvider::class
+    ExamServiceProvider::class,
+    StudentServiceProvider::class,
 ];
