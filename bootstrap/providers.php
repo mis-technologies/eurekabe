90395b218bbe6c200137b32922746dd71732d584
airondevt <airondev@gmail.com>
2024-10-24 13:13:25 +0100

    fixsss

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,7 +1,9 @@
 <?php
 
+use Modules\Admin\Providers\AdminServiceProvider;
+
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\FolioServiceProvider::class,
-    
+    AdminServiceProvider::class
 ];
