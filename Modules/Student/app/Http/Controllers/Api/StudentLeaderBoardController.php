8a42c88637c4ed3c5adfe95c319a75b9f223c7a2
airondevt <airondev@gmail.com>
2024-10-25 11:34:12 +0100

    fixes

--- Modules/Student/app/Http/Controllers/Api/StudentLeaderBoardController.php
+++ Modules/Student/app/Http/Controllers/Api/StudentLeaderBoardController.php
@@ -68,6 +68,8 @@ class StudentLeaderBoardController extends Controller
             ->take(10)
             ->get();
 
+
+            
         return response()->json([
             'success' => true,
             'leaderboard' => $leaderboard
