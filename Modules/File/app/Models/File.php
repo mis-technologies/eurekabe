5abf58a7e739a0b27803d98013fb3afb93ca9e13
airondevt <airondev@gmail.com>
2024-11-18 16:24:12 +0100

    integrate pusher

---
+++ Modules/File/app/Models/File.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Modules\File\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Facades\Storage;
+
+
+class File extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'disk',
+        'entity',
+        'entity_id',
+        'filename',
+        'path',
+        'extension',
+        'mime',
+        'identifier',
+        'size',
+    ];
+
+    public $appends = ['url'];
+
+    protected $hidden = [
+        'disk',
+        'entity',
+        'entity_id',
+        'path',
+    ];
+
+    public function getUrlAttribute(){
+        $path = rawurlencode($this->path);
+        $fileLink =  Storage::disk($this->disk)->path($path);
+        return $fileLink;
+    }
+    
+}
