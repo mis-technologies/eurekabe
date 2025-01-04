1e6638ba75d41e6740ce89b8e7fbbec88d7a23b3
Ejeh Micheal Marvelous <49599133+marvelmikel@users.noreply.github.com>
2025-01-04 03:58:33 +0000

    dark and white logo switch fixed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -126,19 +126,19 @@
 
       <!-- Hero Icons -->
       <div class="hidden md:block absolute top-28 -left-1">
-        <img src="asset/images/designs/hero_blue_arrow.png')}}" alt="" />
+        <img src="{{ asset('asset/images/designs/hero_blue_arrow.png')}}" alt="" />
       </div>    
        
       <div class="absolute top-0 left-0 h-full bg-slate-0 w-full -z-10">
         <div class="particle-container h-full w-full ">
           <div class="particle">
-            <img src="asset/images/particle1.png')}}" alt="">
+            <img src="{{ asset('asset/images/particle1.png')}}" alt="">
           </div>
           <div class="particle">
-            <img src="asset/images/particle2.png')}}" alt="">
+            <img src="{{ asset('asset/images/particle2.png')}}" alt="">
           </div>
           <div class="particle">
-            <img src="asset/images/particle3.png')}}" alt="">
+            <img src="{{ asset('asset/images/particle3.png')}}" alt="">
           </div>         
       </div> 
       </div> 
@@ -195,12 +195,12 @@
       >
         <div class="relative w-full md:w-1/2 z-10">
           <img
-            src="asset/images/low-angle-multiracial-college-students BW 1.png')}}"
+            src="{{ asset('asset/images/low-angle-multiracial-college-students BW 1.png')}}"
             alt=""
             class="w-full h-auto"
           />
           <img
-            src="asset/images/designs/Frame 628898.png')}}"
+            src="{{ asset('asset/images/designs/Frame 628898.png')}}"
             alt=""
             class="absolute top-0 left-0 -translate-y-1/2 -translate-x-2 rounded-full shadow-lg drop-shadow-lg"
           />
@@ -217,31 +217,31 @@
           </p>
           <ul class="grid grid-cols-2 w-full gap-4 md:gap-x-12 mt-2">
             <li class="flex items-center gap-3">
-              <img src="asset/images/icons/Check circle.png')}}" alt="" /> 
+              <img src="{{ asset('asset/images/icons/Check circle.png')}}" alt="" /> 
               <p class="font-medium text-lg opacity-60"> Leadership experience</p>
             </li>
             <li class="flex items-center gap-3">
-              <img src="asset/images/icons/Check circle.png')}}" alt="" />
+              <img src="{{ asset('asset/images/icons/Check circle.png')}}" alt="" />
               <p class="font-medium text-lg opacity-60">Diversity
                 advocacy</p> 
             </li>
             <li class="flex items-center gap-3">
-              <img src="asset/images/icons/Check circle.png')}}" alt="" /> 
+              <img src="{{ asset('asset/images/icons/Check circle.png')}}" alt="" /> 
               <p class="font-medium text-lg opacity-60">Community
                 building</p>
             </li>
             <li class="flex items-center gap-3">
-              <img src="asset/images/icons/Check circle.png')}}" alt="" />
+              <img src="{{ asset('asset/images/icons/Check circle.png')}}" alt="" />
               <p class="font-medium text-lg opacity-60">Impactful
                 contributions</p>
             </li>
             <li class="flex items-center gap-3">
-              <img src="asset/images/icons/Check circle.png')}}" alt="" />
+              <img src="{{ asset('asset/images/icons/Check circle.png')}}" alt="" />
               <p class="font-medium text-lg opacity-60">Communication skills</p>
               
             </li>
             <li class="flex items-center gap-3">
-              <img src="asset/images/icons/Check circle.png')}}" alt="" />
+              <img src="{{ asset('asset/images/icons/Check circle.png')}}" alt="" />
               <p class="font-medium text-lg opacity-60">Influence
                 expansion</p>
             </li>
@@ -253,23 +253,23 @@
               Join the community
             </button>
             <div class="flex flex-col items-center justify-center">
-              <img src="asset/images/designs/Vector6.png')}}" alt=""/>
+              <img src="{{ asset('asset/images/designs/Vector6.png')}}" alt=""/>
             </dv>
           </div>
         </div>
       </div>
       <!-- <div class="absolute top-24 left-0 hidden md:block">
-        <img src="asset/images/designs/who_we_are_i_l.png')}}" alt="" />
+        <img src="{{ asset('asset/images/designs/who_we_are_i_l.png')}}" alt="" />
       </div> -->
       <div class="absolute top-0 left-1/2 rotate-45 -translate-y-8 translate-x-2">
-        <img src="asset/images/designs/purple_top_i.png')}}" alt="" />
+        <img src="{{ asset('asset/images/designs/purple_top_i.png')}}" alt="" />
       </div>
       <div class="absolute bottom-24 right-0 scale-75 hidden md:block">
-        <img src="asset/images/designs/who_we_are_i_r.png')}}" alt="" />
+        <img src="{{ asset('asset/images/designs/who_we_are_i_r.png')}}" alt="" />
       </div>
       <div class="absolute bottom-0 left-0 w-44 h-44">
         <img
-          src="asset/images/designs/who_we_are_ellipse.png')}}"
+          src="{{ asset('asset/images/designs/who_we_are_ellipse.png')}}"
           alt=""
           class="w-full h-full"
         />
@@ -282,7 +282,7 @@
         class="flex items-center gap-4 pr-4 border-[rgba(0,0,0,0.4)] md:border-r-2"
         id="fbI"
       >
-        <img src="asset/images/socials/mingcute_facebook-line_d.png')}}" alt="" />
+        <img src="{{ asset('asset/images/socials/mingcute_facebook-line_d.png')}}" alt="" />
         <div class="count">
           <h1 class="font-semibold text-2xl md:text-[36px]">5K+</h1>
           <p class="font-semibold opacity-50 text-[15px]">followers</p>
@@ -292,7 +292,7 @@
         class="flex items-center gap-4 pr-4 border-[rgba(0,0,0,0.4)] md:border-r-2 dark:border-[rgba(255,255,255,0.4)]"
         id="tgI"
       >
-        <img src="asset/images/socials/telegram_d.png')}}" alt="" />
+        <img src="{{ asset('asset/images/socials/telegram_d.png')}}" alt="" />
         <div class="count">
           <h1 class="font-semibold text-2xl md:text-[36px]">2K+</h1>
           <p class="font-semibold opacity-50 text-[15px]">followers</p>
@@ -302,7 +302,7 @@
         class="flex items-center gap-4 pr-4 border-[rgba(0,0,0,0.4)] md:border-r-2 dark:border-[rgba(255,255,255,0.4)]"
         id="waI"
       >
-        <img src="asset/images/socials/ic_baseline-whatsapp_d.png')}}" alt="" />
+        <img src="{{ asset('asset/images/socials/ic_baseline-whatsapp_d.png')}}" alt="" />
         <div class="count">
           <h1 class="font-semibold text-2xl md:text-[36px]">1M+</h1>
           <p class="font-semibold opacity-50 text-[15px]">subscribers</p>
@@ -312,14 +312,14 @@
         class="flex items-center gap-4 pr-4 border-[rgba(0,0,0,0.4)] md:border-r-2 dark:border-[rgba(255,255,255,0.4)]"
         id="inI"
       >
-        <img src="asset/images/socials/mdi_instagram_d.png')}}" alt="" />
+        <img src="{{ asset('asset/images/socials/mdi_instagram_d.png')}}" alt="" />
         <div class="count">
           <h1 class="font-semibold text-2xl md:text-[36px]">1K+</h1>
           <p class="font-semibold opacity-50 text-[15px]">followers</p>
         </div>
       </div>
       <div class="flex items-center gap-4 pr-4" id="twI">
-        <img src="asset/images/socials/mingcute_twitter-line_d.png')}}" alt="" />
+        <img src="{{ asset('asset/images/socials/mingcute_twitter-line_d.png')}}" alt="" />
         <div class="count">
           <h1 class="font-semibold text-2xl md:text-[36px]">3K+</h1>
           <p class="font-semibold opacity-50 text-[15px]">subscribers</p>
@@ -352,7 +352,7 @@
           <div
             class="flex flex-col items-start p-4 bg-white rounded-2xl gap-4 shadow-primary drop-shadow-lg shadow dark:bg-dark"
           >
-            <img src="asset/images/icons/clarity_note-line.png')}}" alt="" />
+            <img src="{{ asset('asset/images/icons/clarity_note-line.png')}}" alt="" />
             <h5 class="font-bold text-[16px]">Curriculum Development</h5>
             <p class="opacity-50 text-[16px]">
               Shape engaging learning experiences by designing curriculum and
@@ -363,7 +363,7 @@
             class="relative flex flex-col items-start p-4 bg-white rounded-2xl gap-4 shadow-primary drop-shadow-lg shadow dark:bg-dark"
           >
             <img
-              src="asset/images/icons/carbon_license-third-party (1).png')}}"
+              src="{{ asset('asset/images/icons/carbon_license-third-party (1).png')}}"
               alt=""
             />
             <h5 class="font-bold text-[16px]">Event Organization & Management</h5>
@@ -372,13 +372,13 @@
               and inclusive community.
             </p>
             <div class="absolute -bottom-1 right-0 scale-90 translate-y-1/4">
-              <img src="asset/images/designs/x.png')}}" alt="" />
+              <img src="{{ asset('asset/images/designs/x.png')}}" alt="" />
             </div>
           </div>
           <div
             class="flex flex-col items-start p-4 bg-white rounded-2xl gap-4 shadow-primary drop-shadow-lg shadow dark:bg-dark"
           >
-            <img src="asset/images/icons/la_chalkboard-teacher.png')}}" alt="" />
+            <img src="{{ asset('asset/images/icons/la_chalkboard-teacher.png')}}" alt="" />
             <h5 class="font-bold text-[16px]">Student Support & Mentoring</h5>
             <p class="opacity-50 text-[16px]">
               Provide guidance and encouragement to students, helping them
@@ -388,7 +388,7 @@
           <div
             class="flex flex-col items-start p-4 bg-white rounded-2xl gap-4 shadow-primary drop-shadow-lg shadow dark:bg-dark"
           >
-            <img src="asset/images/icons/Vector (3).png')}}" alt="" />
+            <img src="{{ asset('asset/images/icons/Vector (3).png')}}" alt="" />
             <h5 class="font-bold text-[16px]">Community Engagement & Advocacy</h5>
             <p class="opacity-50 text-[16px]">
               Advocate for student needs and collaborate with stakeholders to
@@ -399,16 +399,16 @@
       </div>
 
       <div class="absolute bottom-0 left-0">
-        <img src="asset/images/designs/Vector7.png')}}" alt="" />
+        <img src="{{ asset('asset/images/designs/Vector7.png')}}" alt="" />
       </div>
       <div class="absolute bottom-5 right-0 translate-x-3 hidden md:block">
-        <img src="asset/images/designs/Vector.png')}}" alt="" class="scale-75" />
+        <img src="{{ asset('asset/images/designs/Vector.png')}}" alt="" class="scale-75" />
       </div>
       <div class="absolute top-1/3 left-1/3 scale-75">
-        <img src="asset/images/designs/Vector (1).png')}}" alt="" />
+        <img src="{{ asset('asset/images/designs/Vector (1).png')}}" alt="" />
       </div>
       <div class="absolute top-28 -left-1 hidden md:block">
-        <img src="asset/images/designs/who_we_are_i_l.png')}}" alt="" />
+        <img src="{{ asset('asset/images/designs/who_we_are_i_l.png')}}" alt="" />
       </div>
     </section>
 
@@ -423,7 +423,7 @@
           <div class="img-container relative">
             <div class="h-52 w-52 border-primary border-[8px] translate-y-6"></div>
             <div class=" bg-[#DFDBD7] border-white border-[8px] h-52 w-52 translate-x-6 absolute top-0 right-0 pt-8">
-              <img src="asset/images/Marv.png')}}" alt="" class="w-full h-full">
+              <img src="{{ asset('asset/images/Marv.png')}}" alt="" class="w-full h-full">
             </div>
           </div>
           <div class="w-full md:max-w-64 space-y-2 text-center md:text-left">
@@ -438,7 +438,7 @@
           <div class="img-container relative">
             <div class="h-52 w-52 border-accent border-[8px] translate-y-6"></div>
             <div class=" bg-[#DFDBD7] border-white border-[8px] h-52 w-52 translate-x-6 absolute top-0 right-0 pt-8">
-              <img src="asset/images/Oche.png')}}" alt="" class="w-full h-full">
+              <img src="{{ asset('asset/images/Oche.png')}}" alt="" class="w-full h-full">
             </div>
           </div>
           <div class="w-full md:max-w-64 space-y-2 text-center md:text-left">
@@ -451,7 +451,7 @@
       </div>
       
       <div class="absolute top-0 left-10 md:left-[20rem]">
-        <img src="asset/images/designs/purple_top_i.png')}}" alt="">
+        <img src="{{ asset('asset/images/designs/purple_top_i.png')}}" alt="">
       </div>
      </section>
 
@@ -467,14 +467,14 @@
 
         <div class="flex items-center justify-center gap-8 gap-y-4 flex-col md:flex-row text-white mt-14 max-w-lg ">
           <button class="bg-primary rounded-2xl w-52 p-3 px-5 inline-flex gap-3 items-center transition-all duration-150 ease-in-out hover:bg-opacity-80 hover:scale-105">
-            <img src="asset/images/playStore.png')}}" alt=" ">
+            <img src="{{ asset('asset/images/playStore.png')}}" alt=" ">
             <div class="flex flex-col gap-1 items-start">
               <p class="text-xs font-light">Get it on</p>
               <h1 class="text-sm font-normal">Google Play</h1>
             </div>
           </button>
           <button class="bg-primary rounded-2xl w-52 p-3 px-5 inline-flex gap-3 items-center transition-all duration-150 ease-in-out hover:bg-opacity-80 hover:scale-105">
-            <img src="asset/images/appStore.png')}}" alt=" ">
+            <img src="{{ asset('asset/images/appStore.png')}}" alt=" ">
             <div class="flex flex-col gap-1 items-start">
               <p class="text-xs font-light">Get it on</p>
               <h1 class="text-sm font-normal">Google Play</h1>
@@ -482,25 +482,25 @@
           </button>
         </div>
         <div class="absolute top-8 left-0 scale-75 md:top-0 md:left-6 md:scale-90">        
-          <img src="asset/images/designs/downloadCurve.png')}}" alt="">
+          <img src="{{ asset('asset/images/designs/downloadCurve.png')}}" alt="">
         </div>
       </div>
       
       <div class="absolute bottom-0 right-0 hidden md:block">        
-        <img src="asset/images/designs/elipse.png')}}" alt="">
+        <img src="{{ asset('asset/images/designs/elipse.png')}}" alt="">
       </div>
       </div>
 
       <div class="absolute top-0 left-0 h-full bg-slate-0 w-full -z-10">
         <div class="particle-container h-full w-full ">
           <div class="particle">
-            <img src="asset/images/particle1.png')}}" alt="">
+            <img src="{{ asset('asset/images/particle1.png')}}" alt="">
           </div>
           <div class="particle">
-            <img src="asset/images/particle2.png')}}" alt="">
+            <img src="{{ asset('asset/images/particle2.png')}}" alt="">
           </div>
           <div class="particle">
-            <img src="asset/images/particle3.png')}}" alt="">
+            <img src="{{ asset('asset/images/particle3.png')}}" alt="">
           </div>         
       </div> 
       </div> 
@@ -540,7 +540,7 @@
               </p>
             </div>
             <div class="relative inline-flex justify-end translate-x-8">
-              <img src="asset/images/people-playing-wooden-tower-game 1.png')}}" alt="" class="w-10/12 object-cover" />
+              <img src="{{ asset('asset/images/people-playing-wooden-tower-game 1.png')}}" alt="" class="w-10/12 object-cover" />
             </div>
           </div>
       
@@ -555,7 +555,7 @@
               </p>
             </div>
             <div class="relative inline-flex justify-end translate-x-8">
-              <img src="asset/images/people-playing-wooden-tower-game 1.png')}}" alt="" class="w-10/12 object-cover" />
+              <img src="{{ asset('asset/images/people-playing-wooden-tower-game 1.png')}}" alt="" class="w-10/12 object-cover" />
             </div>
           </div>
           </div>
@@ -572,7 +572,7 @@
               </p>
             </div>
             <div class="relative inline-flex justify-end translate-x-8">
-              <img src="asset/images/people-playing-wooden-tower-game 1.png')}}" alt="" class="w-10/12 object-cover" />
+              <img src="{{ asset('asset/images/people-playing-wooden-tower-game 1.png')}}" alt="" class="w-10/12 object-cover" />
             </div>
           </div>
       
@@ -587,7 +587,7 @@
               </p>
             </div>
             <div class="relative inline-flex justify-end translate-x-8">
-              <img src="asset/images/people-playing-wooden-tower-game 1.png')}}" alt="" class="w-10/12 object-cover" />
+              <img src="{{ asset('asset/images/people-playing-wooden-tower-game 1.png')}}" alt="" class="w-10/12 object-cover" />
             </div>
           </div>
           </div>
