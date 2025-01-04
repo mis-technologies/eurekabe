37cf67070d30cde67d11a92aa4671d2aed2e7ac7
Ejeh Micheal Marvelous <49599133+marvelmikel@users.noreply.github.com>
2025-01-04 04:01:47 +0000

    dark and white logo switch fixed

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -229,7 +229,7 @@
   
       <!-- Footer Socials -->
        <div class="container flex flex-col items-center justify-between py-7 md:flex-row gap-y-10 dark:text-white">
-        <img src="asset/images/logo.png" alt="" class="eurekaLogo md:-translate-x-5 eurekaLogo">
+        <img id="logo" src="{{ asset('asset/images/logo.png')}}" alt="Eureka EdTech" class="" />
         <p class="opacity-50 text-sm text-center">&copy; 2024 Eureka. All rights reveserved. For inquries. contact: info@eureka.com</p>
         <div class="flex items-center gap-6">
           <a
