3e66e14d5b40da3998238fa0cd664b4671f6bfe1
airondevt <airondev@gmail.com>
2025-01-06 10:07:55 +0100

    fix: update user seeder to use updateOrCreate for user records

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -100,7 +100,7 @@ class UserSeeder extends Seeder
         ];
 
         foreach ($users as $user) {
-            User::create($user);
+            User::updateOrCreate(['email' => $user['email']], $user);
         }
     }
 }
