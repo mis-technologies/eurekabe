163201a46a18b8b8b5608128b134e9e99cf8ef97
airondevt <airondev@gmail.com>
2025-01-13 09:13:59 +0100

    Cast interest on user model to array

--- app/Models/User.php
+++ app/Models/User.php
@@ -81,6 +81,7 @@ class User extends Authenticatable implements FilamentUser
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'interest' => 'array',
         ];
     }
 
