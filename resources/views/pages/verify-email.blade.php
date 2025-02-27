832b15e68c5e987f5b387f4937c9409abd22e727
clintonace <clintonace09@gmail.com>
2025-02-27 06:51:24 +0100

    z-index

--- resources/views/pages/verify-email.blade.php
+++ resources/views/pages/verify-email.blade.php
@@ -89,10 +89,10 @@ dd($email);
     // dd($verified);
 @endphp
 
-@if($verified == false)
+@if($verified == true)
 
 
-    <div class="submit_popup z-9999 show" id="submit_popup">
+    <div class="submit_popup z-10 show" id="submit_popup">
 
         <!-- Popup -->
         <div style="display: block" class="submit_success container bg-white rounded-xl w-full h-fit max-w-[300px] md:max-w-[580px] items-center justify-center translate-y-5 py-12 dark:bg-dark relative"
@@ -138,10 +138,10 @@ dd($email);
         }
     });
 </script>
-{{--
+
 <script>
     const dashboardUrl = "{{ route('advocate.dashboard') }}";
-</script> --}}
+</script>
 
 <script src="/asset/src/scripts/main.js"></script>
 
