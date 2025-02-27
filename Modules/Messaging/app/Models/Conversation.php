4c434c9b0277606821fdc68d791079b17b5735b2
airondev <airondev@gmail.com>
2025-02-27 16:55:57 +0100

    fix: refactor Conversation model methods for improved readability and performance

--- Modules/Messaging/app/Models/Conversation.php
+++ Modules/Messaging/app/Models/Conversation.php
@@ -2,10 +2,11 @@
 
 namespace Modules\Messaging\Models;
 
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Modules\Messaging\Models\Message;
 use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
+use Modules\Messaging\Models\Message;
 use Webpatser\Uuid\Uuid;
 
 class Conversation extends Model
@@ -16,28 +17,32 @@ class Conversation extends Model
         'entity', // project, campaing, collaboration, chat, user
         'entity_id', //1101
         'user_id', //1105
-        'uuid' 
+        'uuid',
     ];
 
     public $appends = ['recent_message', 'unread_count', 'heading', 'date', 'is_delivery_request'];
-    
-    public function messages(){
+
+    public function messages()
+    {
         return $this->hasMany(Message::class);
     }
 
-    public function getRecentMessageAttribute(){
+    public function getRecentMessageAttribute()
+    {
         return Message::where('conversation_id', $this->id)->latest()->first();
     }
 
-    public function getIsDeliveryRequestAttribute(){
-        if($this->entity == 'Modules\Trip\Entities\PackageDelivery'){
+    public function getIsDeliveryRequestAttribute()
+    {
+        if ($this->entity == 'Modules\Trip\Entities\PackageDelivery') {
             return true;
-        }else{
+        } else {
             return false;
         }
     }
 
-    public function getQuickActionsAttribute(){
+    public function getQuickActionsAttribute()
+    {
         // $actions = [];
         // if($this->entity === 'Modules\Trip\Entities\PackageDelivery' ){
         //     if($delivery  = PackageDelivery::find($this->entity_id)){
@@ -52,104 +57,87 @@ class Conversation extends Model
         // return $actions;
     }
 
-    public function getUnreadCountAttribute(){
+    public function getUnreadCountAttribute()
+    {
         return Message::where('conversation_id', $this->id)->whereNull('read_at')->count();
     }
-    public function getDateAttribute(){
+    public function getDateAttribute()
+    {
         return $this->created_at->diffForHumans();
     }
 
-
-    public function getHeadingAttribute(){
+    public function getHeadingAttribute()
+    {
         $user = User::find($this->user_id);
         $entityUser = User::find($this->entity_id);
-        
+
         $recentMessage = Message::where('conversation_id', $this->id)->latest()->first();
-        if($recentMessage){
-          
-           $recentToUser = User::find($recentMessage->to_user_id);
-           $recentFromUser = User::find($recentMessage->user_id);
-        
-            if ($this->entity == 'App\Models\User' ) {
-                if( $recentToUser && $recentFromUser){
-                    $user = auth()->user();
-                    if ($user->id == $recentToUser->id ) {
-                        return [
-                            'name' => $recentFromUser->name,
-                            'profile_pic' => $recentFromUser->profile_pic,
-                            'recent_message' => $recentMessage->text,
-                            'recent_message_created_at' => $recentMessage->created_at,
-                        ];
-                    }else {
-                        return [
-                            'name' => $recentToUser->name,
-                            'profile_pic' => $recentToUser->profile_pic,
-                            'recent_message' => $recentMessage->text,
-                            'recent_message_created_at' => $recentMessage->created_at,
-                        ];
-                    }
-        
-                }
-            }
-
-
-
+        $authUserId = Auth::user()->id;
+        if ($user->id == $authUserId) {
+            return [
+                'name' => $entityUser->name,
+                'profile_pic' => $entityUser->profile_pic,
+                'recent_message' => $recentMessage ?? 'New conversation',
+                'recent_message_created_at' => $recentMessage->created_at ?? $this->created_at,
+            ];
+        } else {
+            return [
+                'name' => $user->name,
+                'profile_pic' => $user->profile_pic,
+                'recent_message' => $recentMessage ?? 'New conversation',
+                'recent_message_created_at' => $recentMessage->created_at ?? $this->created_at,
+            ];
         }
 
-
-       
     }
 
-
-    public function getEntityDetailsAttribute(){
-       return app($this->entity)::findOrFail($this->entity_id) ;       
+    public function getEntityDetailsAttribute()
+    {
+        return app($this->entity)::findOrFail($this->entity_id);
     }
 
-    public function user(){
+    public function user()
+    {
         return $this->belongsTo(User::class);
     }
-    public function users(){
+    public function users()
+    {
         return $this->belongsToMany(User::class, 'conversation_user');
     }
-  
+
     public function scopeDm($query)
     {
-        return $query->where('entity', 'App\Models\User' );
+        return $query->where('entity', 'App\Models\User');
     }
 
     public function scopeTicket($query)
     {
-        return $query->where('entity', 'Modules\Support\Entities\SupportTicket' );
+        return $query->where('entity', 'Modules\Support\Entities\SupportTicket');
     }
 
-
-    public function participants(){
-        $messages = collect($this->messages )->unique(function ($item) {
+    public function participants()
+    {
+        $messages = collect($this->messages)->unique(function ($item) {
             return $item->user_id . $item->to_user_id;
         });
 
         $participants = $messages->map(function ($me) {
             return $me->toUser;
         });
-       
 
         return $participants;
     }
-   
 
-    public static function boot(){
+    public static function boot()
+    {
         parent::boot();
-        static::creating(function($conv) {
+        static::creating(function ($conv) {
             $conv->uuid = Uuid::generate(4)->__get('string'); // string, hex, uuid_ordered
         });
 
-        static::retrieved(function($conv) {
-            
-        });
+        static::retrieved(function ($conv) {
 
-       
-        
-        
+        });
 
     }
 }
