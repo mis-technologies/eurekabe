2f4b11f980862d1232aecb41dbe9210cfa090f64
airondev <airondev@gmail.com>
2025-03-01 23:24:01 +0100

    fix: round time_taken calculation to two decimal places in result method for StudentExam model

--- Modules/Student/app/Models/StudentExam.php
+++ Modules/Student/app/Models/StudentExam.php
@@ -88,7 +88,8 @@ class StudentExam extends Model
         $isPassed = $correctPercentage >= $exam->pass_percentage;
 
         // Return a detailed summary of the result
-        $time_taken = $this->started_at ? (double)($this->started_at->diffInMinutes($this->ended_at) ) : 2.00;
+        // $time_taken = $this->started_at ? (double)($this->started_at->diffInMinutes($this->ended_at) ) : 2.00;
+        $time_taken = $this->started_at ? round((double)($this->started_at->diffInMinutes($this->ended_at)), 2) : 2.00;
 
         return [
             'exam_type' => $examType == 1 ? 'mcq' : 'essay',
