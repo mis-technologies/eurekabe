783dc0f64679792be32bf2efce615deae800f711
Ejeh Micheal Marvelous <49599133+marvelmikel@users.noreply.github.com>
2025-03-06 23:25:53 +0000

    feat: add Category and Blog models, migrations, and seeders; update routes and views for blog functionality

---
+++ app/Models/Blog.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Blog extends Model
+{
+    //
+    protected $table = 'blogs';
+    protected $fillable = ['title', 'content','image',  'slug', 'category_id',  'status'];
+}
