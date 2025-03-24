af8ce2e0ce78ea69417051dbf9e251ddf326e903
airondev <airondev@gmail.com>
2025-03-24 08:22:09 +0100

    fix: update participant selection to include user_id as id in StudentChallenge model

--- Modules/Student/app/Models/StudentChallenge.php
+++ Modules/Student/app/Models/StudentChallenge.php
@@ -27,7 +27,7 @@ class StudentChallenge extends Model
     public function participants()
     {
         return $this->belongsToMany(User::class, 'student_challenge_participants', 'challenge_id', 'user_id')
-                    ->select('firstname', 'username', 'lastname', 'image', 'email')
+                    ->select('user_id as id', 'firstname', 'username', 'lastname', 'image', 'email')
                     ->withPivot('status', 'score')
                     ->withTimestamps();
     }
